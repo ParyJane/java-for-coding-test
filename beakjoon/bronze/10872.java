676e80fc791931957c43eb69690625ae5887b00f
JPJ082 <96585009+ParyJane@users.noreply.github.com>
2022-10-25 17:57:59 +0900

    Update 10872.java
    코드 정렬 맞춤

--- beakjoon/bronze/10872.java
+++ beakjoon/bronze/10872.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	  public static int recursion(int n) {
+    public static int recursion(int n) {
       
         if(n == 1 || n == 0) return 1; // 1! = 0! = 1
         return n * recursion(n - 1);   // n! = n * (n - 1)!
