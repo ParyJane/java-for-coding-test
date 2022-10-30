23530f85e1c28f4cd5eddd5547c6ff2b591f9685
JPJ082 <96585009+ParyJane@users.noreply.github.com>
2022-10-30 16:19:42 +0900

    Create AverageValueOfArray1.java

---
+++ programmers/0/AverageValueOfArray1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        for(double num : numbers) {
+            answer += num;
+        }
+        return answer/numbers.length;
+    }
+}
