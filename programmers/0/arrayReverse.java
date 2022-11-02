b913769d46dddb37b67748f418285528f26972e6
JPJ082 <96585009+ParyJane@users.noreply.github.com>
2022-11-02 13:52:03 +0900

    Create arrayReverse.java

---
+++ programmers/0/arrayReverse.java
@@ -0,0 +1,12 @@
+class Solution {
+  
+    public String solution(String my_string) {
+      
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+        String answer = sb.toString();
+        return answer;
+    }
+  
+}
