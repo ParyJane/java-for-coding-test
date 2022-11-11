9fa1b957851b0a5bf99caa93149b0f5cb01d8cc5
JPJ082 <96585009+ParyJane@users.noreply.github.com>
2022-11-11 23:36:06 +0900

    Update 120849.java

--- programmers/0/120849.java
+++ programmers/0/120849.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-				// 'a','e','i','o','u' 를 공백으로 변경
+	// 'a','e','i','o','u' 를 공백으로 변경
         String answer = my_string.replaceAll("[a,e,i,o,u]","");
         return answer;
     }
