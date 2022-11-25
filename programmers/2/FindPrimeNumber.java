dd30fc53fcece733ed14d13fff2225836ce877a3
JPJ082 <96585009+ParyJane@users.noreply.github.com>
2022-11-26 01:35:01 +0900

    Create FindPrimeNumber.java

---
+++ programmers/2/FindPrimeNumber.java
@@ -0,0 +1,61 @@
+import java.util.Set;
+import java.util.HashSet;
+
+/* 프로그래머스 - 소수찾기 */
+class Solution {
+    
+    static String[] arr;
+    static Set<Integer> set = new HashSet<Integer>();
+    
+    public int solution(String numbers) {
+        int answer = 0;
+        arr = numbers.split("");
+        
+        // 숫자 조합 
+        for(int i = 1; i <= arr.length; i++) {
+            permutation(0, arr.length, i);
+        }
+        
+        // 소수가 몇 개인지 찾기
+        for(int num : set) {
+            if(findPrimeNumber(num)) answer++;
+        }
+        
+        return answer;
+    }
+    
+    // 순열
+    static void permutation(int depth, int n, int r) {
+        if(depth == r) {
+            // 조합한 숫자 Set에 저장 
+            String num = "";
+            for(int i = 0; i < r; i++) {
+                num += arr[i];
+            }
+            set.add(Integer.parseInt(num));
+            return;
+        }
+        
+        for(int i = depth; i < n; i++) {
+            swap(depth, i);
+            permutation(depth+1, n, r);
+            swap(depth, i);
+        }
+        
+    }
+    
+    static void swap(int depth, int index) {
+        String temp = arr[depth];
+        arr[depth] = arr[index];
+        arr[index] = temp;
+    }
+    
+    // 소수찾기
+    static boolean findPrimeNumber(int num) {
+        if(num <= 1) return false;
+        for(int i = 2; i * i <= num; i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+}
