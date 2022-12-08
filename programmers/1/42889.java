0ffe0a5c43a4de2551a7b86b5fffa12a0e96b808
JPJ082 <96585009+ParyJane@users.noreply.github.com>
2022-12-08 23:47:10 +0900

    Update 42889.java

--- programmers/1/42889.java
+++ programmers/1/42889.java
@@ -1,42 +1,34 @@
-import java.util.HashMap;
-import java.util.Arrays;
-import java.util.Map.Entry;
-import java.util.List;
-import java.util.Comparator;
-import java.util.stream.Collectors;
-
+import java.util.ArrayList;
+/* 실패율 */
 class Solution {
-    
     public int[] solution(int N, int[] stages) {
-        
         int[] answer = new int[N];
-        HashMap<Integer, Double> map = new HashMap<Integer, Double>();
+        ArrayList<double[]> failureRate = new ArrayList<double[]>();
         
-        Arrays.sort(stages);
-        
-        // 실패율 구하기
+        // 실패율 구하기 
         for(int i = 1; i <= N; i++) {
             double arrive = 0;
-            int clear = 0;            
-            for(int j = i - 1; j < stages.length; j++) {
+            int clear = 0;
+            for(int j = 0; j < stages.length; j++) {
                 if(stages[j] >= i) {
                     if(stages[j] == i) arrive++;
                     clear++;
-                } 
+                }
+            }    
+            if(clear == 0) {
+                failureRate.add(new double[] {i, 0});
+                continue;
             }
-            map.put(i, (clear == 0 ? 0 : arrive / clear));
+            failureRate.add(new double[] {i, arrive/clear});
         }
         
-        // 실패율이 높은 순서대로 정렬
-        List<Entry<Integer, Double>> entries = map.entrySet().stream()
-                                        .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
-                                        .collect(Collectors.toList());
+        // 실패율 내림차순으로 정렬하기 
+        failureRate.sort((a, b) -> Double.compare(b[1], a[1]));
         
         // 출력
-        int index = 0;
-        for(Entry<Integer, Double> entry : entries) {
-            answer[index++] = entry.getKey();
-	    //System.out.println(entry.getKey() + ", " + entry.getValue());
+        for(int i = 0; i < N; i++) {
+            answer[i] = (int)failureRate.get(i)[0];
+            //System.out.println((int)failureRate.get(i)[0] + "/" + failureRate.get(i)[1]);
         }
         
         return answer;
